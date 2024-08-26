abb11f0b8d70f085180a7850d610aa7fb095939a
HildibrandMandervil <171137600+HildibrandMandervil@users.noreply.github.com>
2024-08-26 15:27:22 +0000

    21章終わり

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  resources :users, only: [:show ,:edit]
+
   devise_for :users
-  resources :post_images, only: [:new, :index, :show ,:create ,:destroy]
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'homes/about'=>'homes#about', as: 'about'
   root to: 'homes#top'
+  get 'homes/about'=>'homes#about', as: 'about'
+  resources :post_images, only: [:new, :index, :show ,:create ,:destroy]do
+    resource :favorite, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
+  resources :users, only: [:show ,:edit,:update]
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  
 end
